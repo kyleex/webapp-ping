d68dc918a0b34ad753c594747aa9d52649e811bc
kyleex <antwill34@icloud.com>
2023-11-09 13:40:46 +0100

    v.0.1

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
+const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // Pour analyser les corps de requêtes en JSON
+app.use(express.static(path.join(__dirname, "public"))); // Servir les fichiers statiques depuis 'public'
 
 // Route pour afficher la page d'administration des formulaires
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/admin.html");
+  res.sendFile(path.join(__dirname, "public", "admin.html"));
 });
 
-// aficher
+// Route pour afficher le formulaire
 app.get("/form", (req, res) => {
-  res.sendFile(__dirname + "/form.html");
+  res.sendFile(path.join(__dirname, "public", "form.html"));
 });
 
 // Route pour traiter la soumission du formulaire
@@ -21,13 +25,22 @@ app.post("/submit-form", (req, res) => {
   const formData = req.body;
 
   fs.readFile("form-data.json", (err, data) => {
+    let json = [];
     if (err) {
-      data = "[]";
+      // Si le fichier n'existe pas ou une autre erreur, on part d'un tableau vide
+      console.log(err);
+    } else {
+      // Sinon, on parse les données existantes
+      json = JSON.parse(data.toString());
     }
-    const json = JSON.parse(formData);
     json.push(formData);
     fs.writeFile("form-data.json", JSON.stringify(json, null, 2), (err) => {
-      if (err) throw err;
+      if (err) {
+        console.error(err);
+        return res
+          .status(500)
+          .send("An error occurred while saving form data.");
+      }
       res.send("Form submitted successfully!");
     });
   });
@@ -37,42 +50,21 @@ app.post("/submit-form", (req, res) => {
 app.get("/get-form-data", (req, res) => {
   fs.readFile("form-data.json", (err, formData) => {
     if (err) {
-      res.json([]);
+      if (err.code === "ENOENT") {
+        // Si le fichier n'existe pas, renvoie un tableau vide
+        res.json([]);
+      } else {
+        // Si une autre erreur se produit, renvoie un code d'erreur 500
+        console.error(err);
+        res.status(500).send("An error occurred while reading form data.");
+      }
     } else {
-      res.json(JSON.parse(formData));
+      res.json(JSON.parse(formData.toString()));
     }
   });
 });
 
-// // Routes pour valider et rejeter les formulaires
-// app.post("/validate-form/:index", (req, res) => {
-//   const index = req.params.index;
-//   updateFormStatus(index, "validated", res);
-// });
-
-// app.post("/reject-form/:index", (req, res) => {
-//   const index = req.params.index;
-//   updateFormStatus(index, "rejected", res);
-// });
-
-// // Fonction pour mettre à jour le statut d'un formulaire
-// function updateFormStatus(index, status, res) {
-//   fs.readFile("form-data.json", (err, data) => {
-//     if (err) throw err;
-//     const json = JSON.parse(data);
-//     if (json[index]) {
-//       json[index].status = status;
-//       fs.writeFile("form-data.json", JSON.stringify(json, null, 2), (err) => {
-//         if (err) throw err;
-//         res.send(`Form ${status} successfully!`);
-//       });
-//     } else {
-//       res.status(404).send("Form not found.");
-//     }
-//   });
-// }
-
 // Démarrage du serveur sur le port 3000
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
